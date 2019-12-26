47d5630b8afda55fee44b6c01b9d4fc8c90add2f
coe_mobile_user <coe_mobile_user@COE-iMac.local>
2019-12-26 12:07:33 +0530

    CR:Yogesh::Created response data classes.

--- app/src/main/java/com/example/cityweather/ui/models/WeatherIconUrl.kt
+++ app/src/main/java/com/example/cityweather/ui/models/WeatherIconUrl.kt
@@ -3,15 +3,9 @@ package com.example.cityweather.ui.models
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-
-data class City(
-    @SerializedName("type")
+class WeatherIconUrl(
+    @SerializedName("value")
     @Expose
-    var type: String,
-    @SerializedName("query")
-    @Expose
-    var query: String
-
+    val value: String
 ) {
-
 }
